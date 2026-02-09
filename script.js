86a44796f504fbfe81d63d7bc9bcbf61f49cecf5
thos-1136 <tiago.oliveirasa09@gmail.com>
2026-02-09 19:59:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let contador = 0;
+let newvalue = 0;
+let n1 =0;
+let n2 =0;
 function result() {
-  let n1 = Number(document.getElementById("n1").value);
-  let n2 = Number(document.getElementById("n2").value);
-  let md = document.getElementById("md").value;
   let result = 0;
 
   if (md == "*") result = n1 * n2;
@@ -10,4 +11,29 @@ function result() {
   else if (md == "-") result = n1 - n2;
 
   document.getElementById("saida").innerText = result;
+}
+function mais() {
+  md = "+";
+}
+function menos() {
+  md =  "-";
+}
+function div() {
+  md =  "/"
+}
+function mult() {
+  md = "*"
+}
+function um() {
+  if (contador == 0){
+    n1 = "1";
+    document.getElementById("n1").innerText = n1;
+  }
+  else {
+    newvalue = "1";
+    n1 = n1 + newvalue;
+  }
+}
+function dois() {
+
 }
