1713e36671513a3b4f29b6388d338ed44fad3ed4
thos-1136 <tiago.oliveirasa09@gmail.com>
2026-02-09 19:10:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let n1 = document.getElementById("n1").value;
+let n2 = document.getElementById("n2").value;
+let md = document.getElementById("md").value;
+let result
+
+if (md == *) result = n1 * n2;
+else if (md ==  +) result = n1 + n2;
+else if (md == /) result =  n1 / n2;
+else if (md == -) result = n1 - n2;
+
+document.getElementById("saida").innerText = result;
+
