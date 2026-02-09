5b964cea0bb4749108500de4e834c1ea4ed635c3
thos-1136 <tiago.oliveirasa09@gmail.com>
2026-02-09 19:16:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let n1 = Number(document.getElementById("n1").value);
-let n2 = Number(document.getElementById("n2").value);
-let md = document.getElementById("md").value;
-let result = 0;
+funnction calcular() {
+  let n1 = Number(document.getElementById("n1").value);
+  let n2 = Number(document.getElementById("n2").value);
+  let md = document.getElementById("md").value;
+  let result = 0;
 
-if (md == "*") result = n1 * n2;
-else if (md == "+") result = n1 + n2;
-else if (md == "/") result = n1 / n2;
-else if (md == "-") result = n1 - n2;
+  if (md == "*") result = n1 * n2;
+  else if (md == "+") result = n1 + n2;
+  else if (md == "/") result = n1 / n2;
+  else if (md == "-") result = n1 - n2;
 
-document.getElementById("saida").innerText = result;
+  document.getElementById("saida").innerText = result;
+}
