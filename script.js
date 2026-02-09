dd9e6fdedd208d8e07e69d7b1f4f8148c7b327b0
thos-1136 <tiago.oliveirasa09@gmail.com>
2026-02-09 19:13:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let n1 = document.getElementById("n1").value;
-let n2 = document.getElementById("n2").value;
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
 let md = document.getElementById("md").value;
-let result
+let result = 0;
 
-if (md == *) result = n1 * n2;
-else if (md ==  +) result = n1 + n2;
-else if (md == /) result =  n1 / n2;
-else if (md == -) result = n1 - n2;
+if (md == "*") result = n1 * n2;
+else if (md == "+") result = n1 + n2;
+else if (md == "/") result = n1 / n2;
+else if (md == "-") result = n1 - n2;
 
 document.getElementById("saida").innerText = result;
-
