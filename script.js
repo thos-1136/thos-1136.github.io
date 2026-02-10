d1925443b412627f5ef6265c04ce874e89050d9f
thos-1136 <tiago.oliveirasa09@gmail.com>
2026-02-10 10:10:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,26 +63,194 @@ function um() {
   }
 }
 function dois(){
-
+  if (contador == 0) {
+    n1 = "2";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "2";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "2";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "2";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function tres(){
-
+  if (contador == 0) {
+    n1 = "3";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "3";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "3";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "3";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function quatro(){
-
+  if (contador == 0) {
+    n1 = "4";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "4";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "4";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "4";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function cinco(){
-
+  if (contador == 0) {
+    n1 = "5";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "5";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "5";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "5";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function seis(){
-
+  if (contador == 0) {
+    n1 = "6";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "6";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "6";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "6";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function sete(){
-
+  if (contador == 0) {
+    n1 = "7";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "7";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "7";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "7";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function oito(){
-
+  if (contador == 0) {
+    n1 = "8";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "8";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "8";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "8";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
 function nove(){
-
+  if (contador == 0) {
+    n1 = "9";
+    contador = 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador > 0 && contador < 6767) {
+    newvalue = "9";
+    n1 = n1 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n1").innerText = n1;
+  }
+  else if (contador == 6767) {
+    newvalue = "9";
+    n2 = n2 + newvalue;
+    contador = contador + 1;
+    document.getElementById("n2").innerText = n2;
+  }
+  else if (contador > 6767){
+    newvalue = "9";
+    n2 = n2 + newvalue;
+    document.getElementById("n2").innerText = n2;
+  }
 }
